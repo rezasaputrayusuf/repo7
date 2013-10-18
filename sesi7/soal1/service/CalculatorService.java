348a71e51b91866801034011789cc222b6ab542b
RitaFC <fariarita0@gmail.com>
2013-10-18 15:47:35 +0700

    sesi7

---
+++ sesi7/soal1/service/CalculatorService.java
@@ -0,0 +1,7 @@
+//package service;
+public interface CalculatorService{
+	public Integer tambah (bil1, bil2);
+	public Integer kurang (bil1, bil2);
+	public Integer kali (bil1, bil2);
+	public Integer bagi (bil1, bil2);
+}
